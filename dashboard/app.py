0b77290719defc8c90e1fc5b416c6303a63314b5
AKASH M S <manigarakash@gmail.com>
2026-02-19 20:38:22 +0530

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -6,7 +6,7 @@ import os
 # Page config
 # ---------------------------
 st.set_page_config(
-    page_title="Fraud Detection System",
+    page_title="Cost-Sensitive-Real-Time-Fraud-Detection-Decision-System",
     layout="centered"
 )
 
